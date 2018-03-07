e0efda98927d2b0d8a06664e136e115bf9a3c749
MEHMET ALI IZCI <mmehmetaliizci@hotmail.com>
2018-03-07 21:03:52 +0100

    benhousing/huurwoningnen

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 ## Todos and thoughts:
 
-- write an alarmer through telegram or whatever
-- make it interval
 - you have to create same piece of code for different urls
-- how to solve a problem where the websites are different and you have to write different code piece for each website, maybe get all the hrefs from website and check ?
-- maybe a json field would be better
 - currently it will ring if the first one is changed, so if they add 2 or more, you dont know. you just know there is new activity happening
--- scraperModule/scraperIndex.js
+++ scraperModule/scraperIndex.js
@@ -12,6 +12,11 @@ var deleteFolderRecursive = function(path) {
   }
 };
 
+var getPathName = function(websiteName){
+  var defaultPathName = './websiteSnaps/'
+  return defaultPathName + websiteName + '/'
+}
+
 var fs = require('fs');
 const cheerio = require('cheerio')
 const Telegraf = require('telegraf')
@@ -32,33 +37,152 @@ function main() {
     ctx.telegram.sendMessage(ctx.message.chat.id, `Hello !!`)
   })
 
-  // default path name to copy websites to
+  // generic path name vars
 	var defaultPathName = './websiteSnaps/'
-
-
-  //  ############## Section for a website begins #############
-  var websiteName = 'rdamapartments'
-  var websitePathName = defaultPathName + websiteName + '/'
-  // old website content
+  var websiteName = ''
+  var websitePathName = ''
+  // generic html, html links
 	var oldWebsiteHtml = ''
   var oldFirstHouseLink = ''
   var newWebsiteHtml = ''
   var newFirstHouseLink = ''
+
+  // scraper options
+  var scrape = require('website-scraper');
+  var options= {}
+  //bot related
   var text = ''
+
+
+  //  ############## Section for a RDAM APARTMENTS begins #############
+  // websiteName = 'rdamapartments'
+  // websitePathName = getPathName(websiteName)
+  // // old website content
+	// oldWebsiteHtml = ''
+  // oldFirstHouseLink = ''
+  // newWebsiteHtml = ''
+  // newFirstHouseLink = ''
+  // text = ''
+  // // copy the old website contents if its exists
+	// if (fs.existsSync(websitePathName)) {
+  //   //get the html
+  //   oldWebsiteHtml = cheerio.load(fs.readFileSync(websitePathName+'index.html', 'utf8'));
+  //
+  //   oldFirstHouseLink = oldWebsiteHtml('.housecontainer .houseblockcontainer .houseimage').first().attr('href')
+  //   // remove the folder since new data should be written
+	// 	deleteFolderRecursive(websitePathName);
+	// }
+  //
+  //
+	// options = {
+	// 	urls: ['https://rotterdamapartments.com/en/Rental-apartments'],
+  //   sources: [],
+	// 	directory: websitePathName,
+	// };
+  //
+	// // with promise
+	// scrape(options).then((result) => {
+  //
+  //
+  //   newWebsiteHtml = cheerio.load(result[0].text)
+  //   newFirstHouseLink = newWebsiteHtml('.housecontainer .houseblockcontainer .houseimage').first().attr('href')
+  //
+  //   if (newFirstHouseLink !== oldFirstHouseLink) {
+  //     let text = "There is a new link: "+ newFirstHouseLink
+  //     request('https://api.telegram.org/bot540567822:AAEQjj5l_kIhsfQjMIWopg-Loly0ZpIsrE0/sendMessage?chat_id=503848682&text='+ encodeURI(text), function (error, response, body) {});
+  //   }
+  //   else {
+  //     text = 'No new links for ' + websiteName
+  //     request('https://api.telegram.org/bot540567822:AAEQjj5l_kIhsfQjMIWopg-Loly0ZpIsrE0/sendMessage?chat_id=503848682&text='+ encodeURI(text), function (error, response, body) {});
+  //
+  //   }
+  //   //check if the links match, otherwise alarm !!
+	// 	// console.log(result)
+	// }).catch((err) => {
+	// 	// console.log(err);
+	// });
+  //
+  // //  ############## Section for a RDAM APARTMENTS ends #############
+
+
+  // //  ############## Section for a Benhousing begins #############
+  // websiteName = 'benhousing'
+  // websitePathName = getPathName(websiteName)
+	// oldWebsiteHtml = ''
+  // oldFirstHouseLink = ''
+  // newWebsiteHtml = ''
+  // newFirstHouseLink = ''
+  // text = ''
+  // // copy the old website contents if its exists
+	// if (fs.existsSync(websitePathName)) {
+  //   //get the html
+  //   oldWebsiteHtml = cheerio.load(fs.readFileSync(websitePathName+'index.html', 'utf8'));
+  //
+  //   oldFirstHouseLink = oldWebsiteHtml('#ajcontent .result-item .proc').first().attr('href')
+  //   // remove the folder since new data should be written
+	// 	deleteFolderRecursive(websitePathName);
+	// }
+  //
+  //
+	// options = {
+	// 	urls: ['https://www.benhousing.nl/ons-verhuur-aanbod/'],
+  //   sources: [],
+	// 	directory: websitePathName,
+	// };
+  //
+	// // with promise
+	// scrape(options).then((result) => {
+  //
+  //
+  //   newWebsiteHtml = cheerio.load(result[0].text)
+  //   debugger;
+  //   newFirstHouseLink = newWebsiteHtml('#ajcontent .result-item .proc').first().attr('href')
+  //
+  //   if (newFirstHouseLink !== oldFirstHouseLink) {
+  //     let text = "There is a new link: "+ newFirstHouseLink
+  //     request('https://api.telegram.org/bot540567822:AAEQjj5l_kIhsfQjMIWopg-Loly0ZpIsrE0/sendMessage?chat_id=503848682&text='+ encodeURI(text), function (error, response, body) {});
+  //   }
+  //   else {
+  //     text = 'No new links for ' + websiteName
+  //     request('https://api.telegram.org/bot540567822:AAEQjj5l_kIhsfQjMIWopg-Loly0ZpIsrE0/sendMessage?chat_id=503848682&text='+ encodeURI(text), function (error, response, body) {});
+  //
+  //   }
+  //   //check if the links match, otherwise alarm !!
+	// 	// console.log(result)
+	// }).catch((err) => {
+	// 	console.log(err);
+	// });
+  //
+  // //  ############## Section for a Benhousing ends #############
+
+
+  //  ############## Section for a huurWoningen begins #############
+  websiteName = 'huurWoningen'
+  websitePathName = getPathName(websiteName)
+	oldWebsiteHtml = ''
+  oldFirstHouseLink = ''
+  newWebsiteHtml = ''
+  newFirstHouseLink = ''
+  text = ''
   // copy the old website contents if its exists
 	if (fs.existsSync(websitePathName)) {
     //get the html
     oldWebsiteHtml = cheerio.load(fs.readFileSync(websitePathName+'index.html', 'utf8'));
 
-    oldFirstHouseLink = oldWebsiteHtml('.housecontainer .houseblockcontainer .houseimage').first().attr('href')
+    oldFirstHouseLink = oldWebsiteHtml('.listing-search__listings .listing__link').first().attr('href')
     // remove the folder since new data should be written
 		deleteFolderRecursive(websitePathName);
 	}
 
-	var scrape = require('website-scraper');
-	var options = {
-		urls: ['https://rotterdamapartments.com/en/Rental-apartments'],
+
+	options = {
+		urls: ['https://www.huurwoningen.nl/in/rotterdam/?max_price=1000&number_of_rooms=15&size=50&sort=datetime_created&direction=desc&page=1'],
     sources: [],
+    request: {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19'
+      }
+    },
 		directory: websitePathName,
 	};
 
@@ -67,10 +191,11 @@ function main() {
 
 
     newWebsiteHtml = cheerio.load(result[0].text)
-    newFirstHouseLink = newWebsiteHtml('.housecontainer .houseblockcontainer .houseimage').first().attr('href')
+    debugger;
+    newFirstHouseLink = newWebsiteHtml('.listing-search__listings .listing__link').first().attr('href')
 
     if (newFirstHouseLink !== oldFirstHouseLink) {
-      let text = "There is a new link: "+ newFirstHouseLink
+      let text = "There is a new link: https://www.huurwoningen.nl"+ newFirstHouseLink
       request('https://api.telegram.org/bot540567822:AAEQjj5l_kIhsfQjMIWopg-Loly0ZpIsrE0/sendMessage?chat_id=503848682&text='+ encodeURI(text), function (error, response, body) {});
     }
     else {
@@ -81,11 +206,11 @@ function main() {
     //check if the links match, otherwise alarm !!
 		// console.log(result)
 	}).catch((err) => {
-		// console.log(err);
+		console.log(err);
 	});
 
-  //  ############## Section for a website ends #############
-  
+  //  ############## Section for a huurWoningen ends #############
+
 
 
 }
